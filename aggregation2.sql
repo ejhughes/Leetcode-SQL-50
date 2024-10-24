fb261e13bc1943a70ee09f15eb04f81084c7fd62
Erica Hughes <55588163+ejhughes@users.noreply.github.com>
2024-10-24 09:41:50 -0400

    Create aggregation2.sql

---
+++ aggregation2.sql
@@ -0,0 +1,41 @@
+/*
+Problem:
+Table: Prices
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| start_date    | date    |
+| end_date      | date    |
+| price         | int     |
++---------------+---------+
+(product_id, start_date, end_date) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the price of the product_id in the period from start_date to end_date.
+For each product_id there will be no two overlapping periods. That means there will be no two intersecting periods for the same product_id.
+ 
+
+Table: UnitsSold
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| purchase_date | date    |
+| units         | int     |
++---------------+---------+
+This table may contain duplicate rows.
+Each row of this table indicates the date, units, and product_id of each product sold. 
+ 
+
+Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places. If a product does not have any sold units, its average selling price is assumed to be 0.
+*/
+
+/* Solution */
+SELECT p.product_id
+    , ROUND((SUM(price*units)/SUM(units)),2) average_price
+FROM Prices p
+LEFT JOIN UnitsSold s
+    ON p.product_id=s.product_id 
+WHERE purchase_date >=start_date AND purchase_date <= end_date
+GROUP BY 1
