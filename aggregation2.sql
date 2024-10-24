03504f5fda08a2254e21b95ba735c0645fe60211
Erica Hughes <55588163+ejhughes@users.noreply.github.com>
2024-10-24 09:43:54 -0400

    Update aggregation2.sql
    Updated with alternative solution

--- aggregation2.sql
+++ aggregation2.sql
@@ -39,3 +39,12 @@ LEFT JOIN UnitsSold s
     ON p.product_id=s.product_id 
 WHERE purchase_date >=start_date AND purchase_date <= end_date
 GROUP BY 1
+
+/* Alternative, converting potential Null from the Join into zeros and using the BETWEEN function
+
+SELECT p.product_id, IFNULL(ROUND(SUM(units*price)/SUM(units),2),0) AS average_price
+FROM Prices p LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id AND
+u.purchase_date BETWEEN start_date AND end_date
+group by product_id
+*/
