333d28ba3a856ccab95dc0178d5fbd290d900da0
Erica Hughes <55588163+ejhughes@users.noreply.github.com>
2024-10-31 09:57:57 -0400

    Update aggregation7.sql
    Added alternative solution - no CTE or Window Calc

--- aggregation7.sql
+++ aggregation7.sql
@@ -35,3 +35,13 @@ SELECT
     ROUND(SUM(immediate_order)/COUNT(*)*100,2) immediate_percentage
 FROM cte
 WHERE order_date=first_order_date
+
+/* Alternative
+Select round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
+from Delivery
+where (customer_id, order_date) in (
+  Select customer_id, min(order_date) 
+  from Delivery
+  group by customer_id
+);
+*/
